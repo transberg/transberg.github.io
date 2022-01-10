11b952d2d80ed250c89ee8ed88e5a46650679981
SchaafLaura <97459137+transberg@users.noreply.github.com>
2022-01-10 20:44:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function setup(){
 	createCanvas(400, 400);
+	grid = make2DArray(10,10);
 }
-
 function draw(){
 	background(220);
 	ellipse(frameCount % 300,50,80,80);
+}
+
+function make2DArray(cols, rows) {
+	let arr = new Array(cols);
+	for(let i = 0; i < arr.length; i++){
+		arr[i] = new Array(rows);
+	}
 	
 }
