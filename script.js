81f3bfdf0886a71a41c4e8f2d72a63b5f20fbab6
SchaafLaura <97459137+transberg@users.noreply.github.com>
2022-01-10 21:09:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-function make2DArray(cols, rows) {
-	let arr = new Array(cols);
-	for(let i = 0; i < arr.length; i++){
-		arr[i] = new Array(rows);
-	}	
-	return arr;
-}
-
 let grid;
 let cols;
 let rows;
 let resolution = 40;
+
 function setup(){
 	createCanvas(400, 400);
 	cols = width / resolution;
@@ -21,8 +14,14 @@ function setup(){
 		}
 	}
 }
+
 function draw(){
 	background(0);
+	updateGrid();
+	showGrid();
+}
+
+function showGrid(){
 	for(let i = 0; i < cols; i++){
 		for(let j = 0; j < rows; j++){
 			let w = resolution;
@@ -35,3 +34,39 @@ function draw(){
 	}
 }
 
+function updateGrid(){
+	let next = make2DArray(cols, rows);
+	for(let i = 1; i < cols - 1; i++){
+		for(let j = 1; j < rows - 1; j++){
+			let sum = 0;
+			for(let x = i - 1; i <= i + 1; x++){
+				for(let y = j - 1; y <= j + 1; y++){
+					sum += grid[x][y];
+				}
+			}
+			if(grid[i][j] == 1){
+				if(sum == 3 || sum == 4){
+					next[i][j] = 1;
+				}else{
+					next[i][j] = 0;
+				}
+			} else{
+				if(sum == 3){
+					next[i][j] = 1;
+				}else{
+					next[i][j] = 0;
+				}
+			}
+			
+		}
+	}
+	grid = next;
+}
+
+function make2DArray(cols, rows) {
+	let arr = new Array(cols);
+	for(let i = 0; i < arr.length; i++){
+		arr[i] = new Array(rows);
+	}	
+	return arr;
+}
