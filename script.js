022e83ec16c8efe61fa9ae6c8113c2fcc9a969f4
SchaafLaura <97459137+transberg@users.noreply.github.com>
2022-01-10 20:54:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ function make2DArray(cols, rows) {
 }
 
 let grid;
-let cols = 10;
-let rows = 10;
+let cols;
+let rows;
+let resolution = 40;
 function setup(){
 	createCanvas(400, 400);
+	cols = width / resolution;
+	rows = height / resolution;
 	grid = make2DArray(cols, rows);
 	for(let i = 0; i < cols; i++){
 		for(let j = 0; j < rows; j++){
@@ -19,7 +22,16 @@ function setup(){
 	}
 }
 function draw(){
-	background(220);
-	ellipse(frameCount % 300,50,80,80);
+	background(0);
+	for(let i = 0; i < cols; i++){
+		for(let j = 0; j < rows; j++){
+			let w = resolution;
+			let x = i * resolution;
+			let y = j * resolution;
+			fill(grid[i][j] == 1 ? 0 : 255);
+			noStroke();
+			rect(x, y, w, w);
+		}
+	}
 }
 
